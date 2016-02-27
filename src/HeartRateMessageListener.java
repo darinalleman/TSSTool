8930da47c6e1c8aace2725ae8763422563f5855f
Lance Newman <lancelot316@msn.com>
2016-02-27 16:30:54 -0500

    seperated the logic for parsing out the heart rates from the fit file and the calculation of the TSS.

---
+++ src/HeartRateMessageListener.java
@@ -0,0 +1,27 @@
+import com.garmin.fit.Field;
+import com.garmin.fit.Mesg;
+import com.garmin.fit.MesgListener;
+import com.garmin.fit.MesgNum;
+import com.garmin.fit.RecordMesg;
+
+public class HeartRateMessageListener implements MesgListener {
+	private HeartRateLogger logger;
+
+	public HeartRateMessageListener(HeartRateLogger logger){
+		this.logger = logger;
+	}
+	
+	@Override
+	public void onMesg(Mesg msg) {
+		if (msg.getNum() == MesgNum.RECORD) {
+			Field heartRateField = msg.getField(RecordMesg.HeartRateFieldNum);
+			
+			if (heartRateField != null) {
+				int heartRate = heartRateField.getIntegerValue();
+				
+				logger.log(heartRate);
+			}
+		}
+		
+	}
+}
